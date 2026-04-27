a98e57c07db14891d8a54389f7334a50219e66fc
Andrea Masu <andrea.masu1998@gmail.com>
2026-04-27 16:19:44 +0200

    feat(book-detail): book detail feat

---
+++ src/app/pages/book-detail/book-detail.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { BookService } from '../../services/book.service';
+import { LoanService } from '../../services/loan.service';
+import { BookDetail } from '../../models/book-detail.interface';
+
+@Component({
+  selector: 'app-book-detail',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
+  templateUrl: './book-detail.component.html',
+  styleUrls: ['./book-detail.component.css']
+})
+export class BookDetailComponent implements OnInit {
+  book: BookDetail | null = null;
+  showLoanModal = false;
+  loanMessage = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private bookService: BookService,
+    private loanService: LoanService,
+    private router: Router
+  ) {}
+
+  /* Al caricamento del componente, recupera l'ID del libro dalla route e ne carica i dettagli */
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.bookService.getBookById(id).subscribe({
+      next: (response) => {
+        this.book = response.book;
+      },
+      error: (err) => {
+        alert('Errore nel caricamento del libro');
+        this.router.navigate(['/home']);
+      }
+    });
+  }
+
+  /* Mostra il modal per richiedere un prestito */
+  requestLoan(): void {
+    this.showLoanModal = true;
+  }
+
+  /* Conferma la richiesta di prestito, inviando il messaggio dell'utente come parametro */
+  confirmLoan(): void {
+    if (!this.book) return;
+
+    this.loanService.requestLoan(this.book.id, this.loanMessage).subscribe({
+      next: () => {
+        alert('Richiesta di prestito inviata!');
+        this.showLoanModal = false;
+        this.loanMessage = '';
+      },
+      error: (err) => {
+        alert('Errore: ' + err.error?.error);
+      }
+    });
+  }
+}
